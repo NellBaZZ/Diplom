a7c121808573f45f64dbaa183b6bbaf2edcb8e98
NellBaZZ <117501803+NellBaZZ@users.noreply.github.com>
2024-06-04 17:35:40 +1000

    redactMain

--- main.cpp
+++ main.cpp
@@ -4,16 +4,65 @@
 int main() {
     auto start = std::chrono::high_resolution_clock::now();
 
+    BigInt qwe("12345678901234567890123456789012345678901234567890123456789012345678901234567890");
+    BigInt asd("12345678909654321987654321987654321987654321987654321987654321");
     BigInt a("512");//14002965526155144834555305452685214372866
     BigInt b("512");
     BigInt c("3");
-    BigInt result = a^b;
-    result = result/c;
-    result.print();
-
+    
+    BigInt resultAddInt = qwe + asd;
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
-    std::cout << "Time taken by function: " << duration.count() << " seconds" << std::endl;
+    std::cout << "Time taken by function add:" << duration.count() << " seconds" << std::endl;
+
+    start = std::chrono::high_resolution_clock::now();
+    BigInt resultSubInt = qwe - asd;
+    end = std::chrono::high_resolution_clock::now();
+    duration = end - start;
+    std::cout << "Time taken by function substruct" << duration.count() << " seconds" << std::endl;
+
+    start = std::chrono::high_resolution_clock::now();
+    BigInt resultMulInt = qwe * asd;
+    end = std::chrono::high_resolution_clock::now();
+    duration = end - start;
+    std::cout << "Time taken by function exp: " << duration.count() << " seconds" << std::endl;
+
+    start = std::chrono::high_resolution_clock::now();
+    BigInt resultDivInt = a / b;
+    end = std::chrono::high_resolution_clock::now();
+    duration = end - start;
+    std::cout << "Time taken by function div: " << duration.count() << " seconds" << std::endl;
+
+
+    std::cout << "Addition Int: ";
+    resultAddInt.print();
+    std::cout << "Subtraction Int: ";
+    resultSubInt.print();
+    std::cout << "Multiplication Int: ";
+    resultMulInt.print();
+    std::cout << "Division Int: ";
+    resultDivInt.print();
+
+    std::cout << std::endl;
+
+    BigFloat num1("1234567890.656789123456789123456789123456789123456789123456789");//123.656789123456789123456789123456789123456789123456789
+    BigFloat num2("111111.9654321987654321987654321987654321987654321987654321");//987.9654321987654321987654321987654321987654321987654321
+
+    BigFloat resultAdd = num1 + num2;
+    BigFloat resultSub = num1 - num2;
+    BigFloat resultMul = num1 * num2;
+    BigFloat resultDiv = num1 / num2;
+     
+    std::cout << "Addition Float: ";
+     resultAdd.print();
+    std::cout << "Subtraction FLoat: ";
+     resultSub.print();
+    std::cout << "Multiplication Float: ";
+    resultMul.print();
+    std::cout << "Division Float: ";
+    resultDiv.print();
+
+
 
 #ifdef __clang__
     std::cout << "Compiled with Clang " << __clang_version__ << std::endl;
